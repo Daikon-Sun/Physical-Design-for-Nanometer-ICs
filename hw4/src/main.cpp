9684fec242c178ba3ae57f21099061a3aa4ef604
Daikon-Sun <b03901056@ntu.edu.tw>
2017-06-06 16:34:18 +0800

    finish first time

--- hw4/src/main.cpp
+++ hw4/src/main.cpp
@@ -10,19 +10,31 @@
 
 using namespace std;
 
-void plot(FILE* f, const vector<int>& Xs, const vector<int>& Ys,
-          const vector<tuple<int, int, int>>& edges) {
+inline void plot_rect_line(FILE *f, int x0, int y0, int x1, int y1) {
+  if(x0 == x1 || y0 == y1)
+    fprintf(f, "set arrow from %d,%d to %d,%d nohead\n", x0, y0, x1, y1); 
+  else {
+    fprintf(f, "set arrow from %d,%d to %d,%d nohead\n", x0, y0, x0, y1);
+    fprintf(f, "set arrow from %d,%d to %d,%d nohead\n", x0, y1, x1, y1); 
+  }
+}
+void plot(FILE* f, const int& numPins,
+          const vector<int>& Xs, const vector<int>& Ys,
+          const vector<vector<bool>>& T) {
   fprintf(f, "set size ratio -1\nset nokey\n");
-  fprintf(f, "set style line 1 lc rgb '#0060ad' pt 6\n");
-  for(uint i = 0; i < edges.size(); ++i)
-    fprintf(f, "set arrow from %d,%d to %d,%d nohead\n", Xs[get<1>(edges[i])],
-            Ys[get<1>(edges[i])], Xs[get<2>(edges[i])], Ys[get<2>(edges[i])]);
+  fprintf(f, "set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7\n");
+  for(uint i = 0; i < T.size(); ++i) for(uint j = i+1; j < T.size(); ++j)
+    if(T[i][j])
+      plot_rect_line(f, Xs[i], Ys[i], Xs[j], Ys[j]);
   //for(uint i = 0; i < Xs.size(); ++i)
   //  fprintf(f, "set label \"(%d,%d)\" at %d,%d\n", 
   //          Xs[i], Ys[i], Xs[i]+1, Ys[i]);
-  fprintf(f, "plot '-' pt 7 ps 1.3\n");
-  for(uint i = 0; i < Xs.size(); ++i)
-    fprintf(f, "%d %d\n", Xs[i], Ys[i]);
+  fprintf(f, "set style line 1 lc rgb 'blue' pt 5\n");
+  fprintf(f, "set style line 2 lc rgb 'red' pt 7\n");
+  fprintf(f, "plot '-' w p ls 1, '-' w p ls 2\n");
+  for(int i = 0; i < numPins; ++i) fprintf(f, "%d %d\n", Xs[i], Ys[i]);
+  fprintf(f, "e\n");
+  for(uint i = numPins; i < Xs.size(); ++i) fprintf(f, "%d %d\n", Xs[i], Ys[i]);
   fprintf(f, "e\npause -1\n");
 }
 inline int dist(const int& x0, const int& y0, const int& x1, const int& y1) {
@@ -111,12 +123,28 @@ inline int calc_cost(int x0, int y0, int x1, int y1, int x2, int y2) {
   return rtn;
 }
 inline int calc_gain(const vector<int>& Xs, const vector<int>& Ys,
-                     const int& numPins, const vector<pair<int, int>>& MBT,
+                     const int& numPins, const vector<pair<int, int>>& Tedge,
                      const tuple<int, int, int, int, int>& t) {
-  const int &w = get<0>(t), &u0 = get<0>(t), &v0 = get<2>(t), &x = get<3>(t);
-  const int &u1 = MBT[x - numPins].first, &v1 = MBT[x - numPins].second;
+  const int &w = get<0>(t), &u0 = get<1>(t), &v0 = get<2>(t), &x = get<3>(t);
+  assert(x >= numPins);
+  const int &u1 = Tedge[x - numPins].first, &v1 = Tedge[x - numPins].second;
+  assert(u0 < numPins && v0 < numPins && u1 < numPins && v1 < numPins);
   return dist(Xs[u1], Ys[u1], Xs[v1], Ys[v1]) -
          calc_cost(Xs[w], Ys[w], Xs[u0], Ys[u0], Xs[v0], Ys[v0]);
+}
+inline pair<int, int> gen_point(int x0, int y0, int x1, int y1, int x2, int y2) {
+  int mxx = max(x1, x2), mnx = min(x1, x2);
+  int mxy = max(y1, y2), mny = min(y1, y2);
+  if(x0 <= mxx && x0 >= mnx && y0 <= mxy && y0 >= mny)
+    return {INT_MIN, INT_MIN};
+  int rtnx, rtny;
+  if(x0 < mnx) rtnx = mnx;
+  else if(x0 > mxx) rtnx = mxx;
+  else rtnx = x0;
+  if(y0 < mny) rtny = mny;
+  else if(y0 > mxy) rtny = mxy;
+  else rtny = y0;
+  return {rtnx, rtny};
 }
 int main(int argc, char** argv) {
   assert(argc <= 4);
@@ -127,6 +155,8 @@ int main(int argc, char** argv) {
   int numPins;
   fscanf(fin, "NumPins = %d\n", &numPins);
   vector<int> Xs(numPins), Ys(numPins), X_pls_Y(numPins), X_mns_Y(numPins);
+  Xs.reserve(2*numPins);
+  Ys.reserve(2*numPins);
   for(int i = 0; i < numPins; ++i) {
     fscanf(fin, "PIN %*s (%d,%d)\n", &Xs[i], &Ys[i]);
     X_pls_Y[i] = Xs[i] + Ys[i];
@@ -158,7 +188,7 @@ int main(int argc, char** argv) {
   int edge_id = 0;
   vector<vector<bool>> T(2*numPins, vector<bool>(2*numPins));
   vector<vector<tuple<int, int, int>>> Qs(numPins);
-  vector<pair<int, int>> MBT(numPins - 1);
+  vector<pair<int, int>> MBT(numPins - 1), Tedge(numPins - 1);
   for(auto& edge : edges) {
     int &u = get<1>(edge), &v = get<2>(edge);
     int ru = ds1.root(u), rv = ds1.root(v);
@@ -177,6 +207,7 @@ int main(int argc, char** argv) {
           Qs[w].emplace_back(v, v, u), Qs[v].emplace_back(w, v, u);
       }
       MBT[edge_id] = {ru, rv};
+      Tedge[edge_id] = {u, v};
       ds1.unite(edge_id + numPins, ds1.unite(ru, rv));
       ++edge_id;
     }
@@ -187,21 +218,34 @@ int main(int argc, char** argv) {
   vector<bool> colors(2*numPins - 1);
   vector<int> ancs(2*numPins - 1, -1);
   tarjan(edge_id + numPins - 1, numPins, ds2, Qs, MBT, ancs, colors, ans);
-  for(auto &an : ans) get<4>(an) = calc_gain(Xs, Ys, numPins, MBT, an);
+  for(auto &an : ans) get<4>(an) = calc_gain(Xs, Ys, numPins, Tedge, an);
   sort(ans.begin(), ans.end(),
        [&](const tuple<int, int, int, int, int>& t1,
            const tuple<int, int, int, int, int>& t2) {
-         return get<4>(t1) > get<4>(t2);
-       });
-  int total = 0, Tcnt = numPins;
+         return get<4>(t1) > get<4>(t2); });
+  int Tcnt = numPins;
   for(auto &an : ans) {
     if(get<4>(an) <= 0) break;
-    const int &w = get<0>(an), &u0 = get<0>(an), &v0 = get<2>(an);
-    const int &x = get<3>(an), &u1 = MBT[x - numPins].first;
-    const int &v1 = MBT[x - numPins].second;
+    const int &w = get<0>(an), &u0 = get<1>(an), &v0 = get<2>(an);
+    const int &x = get<3>(an), &u1 = Tedge[x - numPins].first;
+    const int &v1 = Tedge[x - numPins].second;
+    assert(x >= numPins);
+    assert(u0 < numPins && v0 < numPins && u1 < numPins && v1 < numPins);
+    assert(T[u0][v0] == T[v0][u0] && T[u1][v1] == T[v1][u1]);
+    if(!T[u0][v0] || !T[u1][v1]) continue;
+    T[u0][v0] = T[v0][u0] = T[u1][v1] = T[v1][u1] = false;
+    int nx, ny;
+    tie(nx, ny) = gen_point(Xs[w], Ys[w], Xs[u0], Ys[u0], Xs[v0], Ys[v0]);
+    if(nx != INT_MIN && ny != INT_MIN) {
+      Xs.push_back(nx);
+      Ys.push_back(ny);
+      T[Tcnt][w] = T[w][Tcnt] = T[Tcnt][u0] = T[u0][Tcnt] = true;
+      T[Tcnt][v0] = T[v0][Tcnt] = true;
+      ++Tcnt;
+    }
   }
   if(argc == 4) {
     FILE *fplt = fopen(argv[3], "w");
-    plot(fplt, Xs, Ys, edges);
+    plot(fplt, numPins, Xs, Ys, T);
   }
 }
