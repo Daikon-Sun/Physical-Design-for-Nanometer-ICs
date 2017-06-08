a1696292e8cc1ff6124b54a6d4403289907ba6f9
Daikon-Sun <b03901056@ntu.edu.tw>
2017-06-08 13:58:10 +0800

    improve more

--- hw4/src/main.cpp
+++ hw4/src/main.cpp
@@ -25,7 +25,7 @@ inline LL dist(const int& x0, const int& y0, const int& x1, const int& y1) {
 inline double dist2(const int& x0, const int& y0, const int& x1, const int& y1) {
   return (x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1);
 }
-template<typename U> inline LL plot(FILE* f, const U& numPins,
+template<typename U> inline LL plot(FILE* f, const U& nPins,
                const vector<int>& Xs, const vector<int>& Ys,
                const vector<vector<bool>>& T) {
   LL cost = 0;
@@ -41,9 +41,9 @@ template<typename U> inline LL plot(FILE* f, const U& numPins,
     fprintf(f, "set style line 1 lc rgb 'blue' pt 5\n");
     fprintf(f, "set style line 2 lc rgb 'red' pt 7\n");
     fprintf(f, "plot '-' w p ls 1, '-' w p ls 2\n");
-    for(int i = 0; i < numPins; ++i) fprintf(f, "%d %d\n", Xs[i], Ys[i]);
+    for(int i = 0; i < nPins; ++i) fprintf(f, "%d %d\n", Xs[i], Ys[i]);
     fprintf(f, "e\n");
-    for(uint i = numPins; i < Xs.size(); ++i) fprintf(f, "%d %d\n", Xs[i], Ys[i]);
+    for(uint i = nPins; i < Xs.size(); ++i) fprintf(f, "%d %d\n", Xs[i], Ys[i]);
     fprintf(f, "e\npause -1\n");
   }
   return cost;
@@ -100,26 +100,25 @@ template<typename U> struct disjoint_set2 {
   vector<U> par;
 };
 template<typename U>
-inline void tarjan(const U& u, const U& numPins, disjoint_set2<U>& ds2, 
-            vector<vector<tuple<U, U, U>>>& Qs, 
-            const vector<pair<U, U>>& MBT, vector<U>& ancs, 
-            vector<bool>& colors, vector<tuple<U, U, U, U, int>>& ans) {
+inline void tarjan(const U& u, const U& nPins, disjoint_set2<U>& ds2, 
+            vector<vector<tuple<U, U>>>& Qs, const vector<pair<U, U>>& MBT, 
+            const vector<pair<U, U>>& Tedge, vector<U>& ancs, 
+            vector<bool>& colors, vector<tuple<U, U, U, int>>& ans) {
   ancs[u] = u;
-  if(u >= numPins) {
-    tarjan(MBT[u - numPins].first,  numPins, ds2, Qs, MBT, ancs, colors, ans);
-    ds2.unite(u, MBT[u - numPins].first);
+  if(u >= nPins) {
+    tarjan(MBT[u - nPins].first,  nPins, ds2, Qs, MBT, Tedge, ancs, colors, ans);
+    ds2.unite(u, MBT[u - nPins].first);
     ancs[ds2.root(u)] = u;
-    tarjan(MBT[u - numPins].second, numPins, ds2, Qs, MBT, ancs, colors, ans);
-    ds2.unite(u, MBT[u - numPins].second);
+    tarjan(MBT[u - nPins].second, nPins, ds2, Qs, MBT, Tedge, ancs, colors, ans);
+    ds2.unite(u, MBT[u - nPins].second);
     ancs[ds2.root(u)] = u;
   }
   colors[u] = true;
-  if(u < numPins) for(auto &Q : Qs[u]) if(colors[get<0>(Q)]) {
-    if(u == get<1>(Q))
-      ans.emplace_back(get<0>(Q), get<1>(Q), get<2>(Q),
-                       ancs[ds2.root(get<0>(Q))], 0);
-    else ans.emplace_back(u, get<1>(Q), get<2>(Q), ancs[ds2.root(get<0>(Q))], 0);
-  }
+  if(u < nPins) for(auto &Q : Qs[u]) if(colors[get<0>(Q)])
+    if(u == Tedge[get<1>(Q)].first || u == Tedge[get<1>(Q)].second)
+      ans.emplace_back(get<0>(Q), get<1>(Q), ancs[ds2.root(get<0>(Q))], 0);
+    else
+      ans.emplace_back(u, get<1>(Q), ancs[ds2.root(get<0>(Q))], 0);
 }
 inline int calc_cost(int x0, int y0, int x1, int y1, int x2, int y2) {
   int rtn = 0, mxx = max(x1, x2), mnx = min(x1, x2);
@@ -132,38 +131,33 @@ inline int calc_cost(int x0, int y0, int x1, int y1, int x2, int y2) {
 }
 template<typename U>
 inline int calc_gain(const vector<int>& Xs, const vector<int>& Ys,
-                     const U& numPins, const vector<pair<U, U>>& Tedge,
-                     const tuple<U, U, U, U, int>& t) {
-  const U &w = get<0>(t), &u0 = get<1>(t), &v0 = get<2>(t), &x = get<3>(t);
-  //assert(x >= numPins);
-  const U &u1 = Tedge[x - numPins].first, &v1 = Tedge[x - numPins].second;
-  //assert(u0 < numPins && v0 < numPins && u1 < numPins && v1 < numPins);
+                     const U& nPins, const vector<pair<U, U>>& Tedge,
+                     const tuple<U, U, U, int>& t) {
+  const U &w = get<0>(t), &u0 = Tedge[get<1>(t)].first;
+  const U &v0 = Tedge[get<1>(t)].second, &x = get<2>(t);
+  const U &u1 = Tedge[x - nPins].first, &v1 = Tedge[x - nPins].second;
   return dist(Xs[u1], Ys[u1], Xs[v1], Ys[v1]) -
          calc_cost(Xs[w], Ys[w], Xs[u0], Ys[u0], Xs[v0], Ys[v0]);
 }
 inline pair<int, int> gen_point(int x0, int y0, int x1, int y1, int x2, int y2) {
   int mxx = max(x1, x2), mnx = min(x1, x2);
   int mxy = max(y1, y2), mny = min(y1, y2);
-  if(x0 <= mxx && x0 >= mnx && y0 <= mxy && y0 >= mny)
-    return {INT_MIN, INT_MIN};
-  int rtnx, rtny;
+  int rtnx = x0, rtny = y0;
   if(x0 < mnx) rtnx = mnx;
   else if(x0 > mxx) rtnx = mxx;
-  else rtnx = x0;
   if(y0 < mny) rtny = mny;
   else if(y0 > mxy) rtny = mxy;
-  else rtny = y0;
   return {rtnx, rtny};
 }
 template<typename U>
 inline LL steiner_tree(vector<int>& Xs, vector<int>& Ys, vector<int>& X_pls_Y,
                           vector<int>& X_mns_Y, vector<vector<bool>>& T) {
-  U numPins = (U)Xs.size();
+  U nPins = (U)Xs.size();
   //construct spanning graph
   vector<tuple<int, U, U>> edges;
-  vector<vector<U>> adj(numPins);
-  edges.reserve(3 * numPins);
-  vector<U> ord_pls(numPins), ord_mns(numPins);
+  vector<vector<U>> adj(nPins);
+  edges.reserve(3 * nPins);
+  vector<U> ord_pls(nPins), ord_mns(nPins);
   iota(ord_pls.begin(), ord_pls.end(), 0);
   iota(ord_mns.begin(), ord_mns.end(), 0);
   sort(ord_pls.begin(), ord_pls.end(), [&](U& p1, U& p2) {
@@ -178,16 +172,16 @@ inline LL steiner_tree(vector<int>& Xs, vector<int>& Ys, vector<int>& X_pls_Y,
   span(Xs, Ys, ord_mns, X_pls_Y, less_y, less<int>(), edges, adj, false);
   span(Xs, Ys, ord_mns, X_pls_Y, grtr_x, greater_equal<int>(), edges, adj, true);
   //construct steiner-tree
-  disjoint_set1<U> ds1(2*numPins - 1); 
+  disjoint_set1<U> ds1(2*nPins - 1); 
   sort(edges.begin(), edges.end(), 
        [&](const tuple<int, U, U>& t0, const tuple<int, U, U>& t1) {
        return get<0>(t0) > get<0>(t1); });
   reverse(edges.begin(), edges.end());
   U edge_id = 0;
   T.clear();
-  T.resize(1.4*numPins, vector<bool>(1.4*numPins, false));
-  vector<vector<tuple<U, U, U>>> Qs(numPins);
-  vector<pair<U, U>> MBT(numPins - 1), Tedge(numPins - 1);
+  T.resize(1.4*nPins, vector<bool>(1.4*nPins, false));
+  vector<vector<tuple<U, U>>> Qs(nPins);
+  vector<pair<U, U>> MBT(nPins - 1), Tedge(nPins - 1);
   LL MST_cost = 0;
   for(auto& edge : edges) {
     U &u = get<1>(edge), &v = get<2>(edge);
@@ -197,52 +191,49 @@ inline LL steiner_tree(vector<int>& Xs, vector<int>& Ys, vector<int>& X_pls_Y,
       T[u][v] = T[v][u] = true;
       for(auto& w : adj[u]) if(w != v) {
         if(ds1.same(ru, w)) 
-          Qs[w].emplace_back(u, u, v), Qs[u].emplace_back(w, u, v);
+          Qs[w].emplace_back(u, edge_id), Qs[u].emplace_back(w, edge_id);
         else if(ds1.same(rv, w)) 
-          Qs[w].emplace_back(v, v, u), Qs[v].emplace_back(w, v, u);
+          Qs[w].emplace_back(v, edge_id), Qs[v].emplace_back(w, edge_id);
         else {
-          Qs[w].emplace_back(u, u, v), Qs[u].emplace_back(w, u, v);
-          Qs[w].emplace_back(v, v, u), Qs[v].emplace_back(w, v, u);
+          Qs[w].emplace_back(u, edge_id), Qs[u].emplace_back(w, edge_id);
+          Qs[w].emplace_back(v, edge_id), Qs[v].emplace_back(w, edge_id);
         }
       }
       for(auto& w : adj[v]) if(w != u) {
         if(ds1.same(ru, w)) 
-          Qs[w].emplace_back(u, u, v), Qs[u].emplace_back(w, u, v);
+          Qs[w].emplace_back(u, edge_id), Qs[u].emplace_back(w, edge_id);
         else if(ds1.same(rv, w)) 
-          Qs[w].emplace_back(v, v, u), Qs[v].emplace_back(w, v, u);
+          Qs[w].emplace_back(v, edge_id), Qs[v].emplace_back(w, edge_id);
         else {
-          Qs[w].emplace_back(u, u, v), Qs[u].emplace_back(w, u, v);
-          Qs[w].emplace_back(v, v, u), Qs[v].emplace_back(w, v, u);
+          Qs[w].emplace_back(u, edge_id), Qs[u].emplace_back(w, edge_id);
+          Qs[w].emplace_back(v, edge_id), Qs[v].emplace_back(w, edge_id);
         }
       }
       MBT[edge_id] = {ru, rv};
       Tedge[edge_id] = {u, v};
-      ds1.unite(edge_id + numPins, ds1.unite(ru, rv));
+      ds1.unite(edge_id + nPins, ds1.unite(ru, rv));
       ++edge_id;
     }
   }
-  disjoint_set2<U> ds2(2*numPins - 1);
-  vector<tuple<U, U, U, U, int>> ans;
-  ans.reserve(1.3*numPins);
-  vector<bool> colors(2*numPins - 1);
-  vector<U> ancs(2*numPins - 1, -1);
-  tarjan(U(edge_id + numPins - 1), numPins, ds2, Qs, MBT, ancs, colors, ans);
+  disjoint_set2<U> ds2(2*nPins - 1);
+  vector<tuple<U, U, U, int>> ans;
+  ans.reserve(1.3*nPins);
+  vector<bool> colors(2*nPins - 1);
+  vector<U> ancs(2*nPins - 1, -1);
+  tarjan(U(edge_id + nPins - 1), nPins, ds2, Qs, MBT, Tedge, ancs, colors, ans);
   #pragma omp parallel for
   for(uint i = 0; i < ans.size(); ++i)
-    get<4>(ans[i]) = calc_gain(Xs, Ys, numPins, Tedge, ans[i]);
+    get<3>(ans[i]) = calc_gain(Xs, Ys, nPins, Tedge, ans[i]);
   sort(ans.begin(), ans.end(),
-       [&](const tuple<U, U, U, U, int>& t1,
-           const tuple<U, U, U, U, int>& t2) {
-         return get<4>(t1) > get<4>(t2); });
-  U Tcnt = numPins;
+       [&](const tuple<U, U, U, int>& t1, const tuple<U, U, U, int>& t2) {
+         return get<3>(t1) > get<3>(t2); });
+  U Tcnt = nPins;
   for(auto &an : ans) {
-    if(get<4>(an) <= 0) break;
-    const U &w = get<0>(an), &u0 = get<1>(an), &v0 = get<2>(an);
-    const U &x = get<3>(an), &u1 = Tedge[x - numPins].first;
-    const U &v1 = Tedge[x - numPins].second;
-    //assert(x >= numPins);
-    //assert(u0 < numPins && v0 < numPins && u1 < numPins && v1 < numPins);
-    //assert(T[u0][v0] == T[v0][u0] && T[u1][v1] == T[v1][u1]);
+    if(get<3>(an) <= 0) break;
+    const U &w = get<0>(an);
+    const U &u0 = Tedge[get<1>(an)].first, &v0 = Tedge[get<1>(an)].second;
+    const U &u1 = Tedge[get<2>(an) - nPins].first;
+    const U &v1 = Tedge[get<2>(an) - nPins].second;
     if(!T[u0][v0] || !T[u1][v1]) continue;
     int nx, ny;
     tie(nx, ny) = gen_point(Xs[w], Ys[w], Xs[u0], Ys[u0], Xs[v0], Ys[v0]);
@@ -264,21 +255,20 @@ int main(int argc, char** argv) {
   int MINX, MINY, MAXX, MAXY;
   FILE *fin = fopen(argv[1] ,"r");
   fscanf(fin, "Boundary = (%d,%d), (%d,%d)\n", &MINX, &MINY, &MAXX, &MAXY);
-  int numPins;
-  fscanf(fin, "NumPins = %d\n", &numPins);
-  vector<int> Xs(numPins), Ys(numPins), X_pls_Y(numPins), X_mns_Y(numPins);
-  Xs.reserve((iter*0.3 + 1)*numPins);
-  Ys.reserve((iter*0.3 + 1)*numPins);
-  X_pls_Y.reserve((iter*0.3 + 1)*numPins);
-  X_mns_Y.reserve((iter*0.3 + 1)*numPins);
-  for(int i = 0; i < numPins; ++i) {
+  int nPins;
+  fscanf(fin, "NumPins = %d\n", &nPins);
+  vector<int> Xs(nPins), Ys(nPins), X_pls_Y(nPins), X_mns_Y(nPins);
+  Xs.reserve((iter*0.3 + 1)*nPins);
+  Ys.reserve((iter*0.3 + 1)*nPins);
+  X_pls_Y.reserve((iter*0.3 + 1)*nPins);
+  X_mns_Y.reserve((iter*0.3 + 1)*nPins);
+  for(int i = 0; i < nPins; ++i) {
     fscanf(fin, "PIN %*s (%d,%d)\n", &Xs[i], &Ys[i]);
     X_pls_Y[i] = Xs[i] + Ys[i];
     X_mns_Y[i] = Xs[i] - Ys[i];
   }
-  bool use_short = (6 * numPins >= 255);
-  bool use_int = (6 * numPins >= 65535);
-  //use_int = true;
+  bool use_short = (6 * nPins >= 255);
+  bool use_int = (6 * nPins >= 65535);
   vector<vector<bool>> T;
   LL orig_MST_cost, MRST_cost;
   for(int i = 0; i < iter; ++i) {
@@ -292,10 +282,11 @@ int main(int argc, char** argv) {
     if(!i) orig_MST_cost = MST_cost;
     FILE *fplt = (argc == 4 ? 
                   fopen((string(argv[3]) + to_string(i)).c_str(), "w") : 0);
-    MRST_cost = plot(fplt, numPins, Xs, Ys, T);
+    MRST_cost = plot(fplt, nPins, Xs, Ys, T);
+    cerr << MRST_cost << endl;
   }
   FILE *fout = fopen(argv[2], "w");
-  fprintf(fout, "NumRoutedPins = %d\n", numPins);
+  fprintf(fout, "NumRoutedPins = %d\n", nPins);
   fprintf(fout, "Wirelength = %lld\n", MRST_cost);
   #pragma omp parallel for
   for(uint i = 0; i < T.size(); ++i)
@@ -314,5 +305,5 @@ int main(int argc, char** argv) {
   cerr << "MRST cost " << MRST_cost << endl;
   cerr << "improvement "
        << double(orig_MST_cost - MRST_cost) / orig_MST_cost << endl;
-  cerr << "numPins " << Xs.size() << endl;
+  cerr << "nPins " << Xs.size() << endl;
 }
